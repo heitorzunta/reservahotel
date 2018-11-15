d9599083bbc27846d25ea2d60779009a1e7f3230
Heitor Batistela Zunta <heitor@basenetworks.com.br>
2018-11-15 11:05:11 -0300

    Projeto inicial sem programa, somente a classe

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,58 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+	private Integer roomNumber;
+	private Date checkinDate;
+	private Date checkoutDate;
+	public static SimpleDateFormat simpleDate = new SimpleDateFormat("dd/MM/yyyy");
+	public Reservation() {
+		
+	}
+
+	public Reservation(Integer roomNumber, Date checkinDate, Date checkoutDate) {
+		super();
+		this.roomNumber = roomNumber;
+		this.checkinDate = checkinDate;
+		this.checkoutDate = checkoutDate;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckinDate() {
+		return checkinDate;
+	}
+
+	public Date getCheckoutDate() {
+		return checkoutDate;
+	}
+
+	public Integer duration() {
+		long durationTime = checkoutDate.getTime() - checkinDate.getTime();
+		return (int) TimeUnit.DAYS.convert(durationTime, TimeUnit.MILLISECONDS); //converter milissegundos em dia
+	}
+	
+	public void updateDates(Date checkinDate, Date checkoutDate) {
+		this.checkinDate = checkinDate;
+		this.checkoutDate = checkoutDate;	
+	}
+	
+	@Override
+	public String toString() {
+		return "Reservation: Room" + roomNumber +
+				", checkin: " + simpleDate.format(checkinDate) +
+				", check-ou: " +simpleDate.format(checkoutDate) +
+				duration() + "nights";	
+	}
+	
+}
